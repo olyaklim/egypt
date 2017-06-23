8daa6d45e81f056c34d157991747fc91108a9e69
Olya <olya_klim@ukr.net>
2017-06-23 14:59:52 +0300

    1-й комит

---
+++ script.js
@@ -0,0 +1,14 @@
+﻿
+$("#form").submit(function(e) {
+    $.ajax({
+           type: "POST",
+           url: "email.php",
+           data: $("#form").serialize(),
+           success: function(responce)
+           {
+                $('#result').html('Результат: ' + responce); 
+           }
+         });
+
+    e.preventDefault();
+});
